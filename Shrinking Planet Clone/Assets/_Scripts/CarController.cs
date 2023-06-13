c0d3af71cc9d442acf01ea3daaded8368e2cace4
Daniel Andrews <daniel.korienko@gmail.com>
2023-06-13 12:36:01 +0300

    Elevation and Azimuth approach next

--- Shrinking Planet Clone/Assets/_Scripts/CarController.cs
+++ Shrinking Planet Clone/Assets/_Scripts/CarController.cs
@@ -1,33 +1,41 @@
+using System;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    private const string HORIZONTAL = "Horizontal";
+
     [SerializeField] private Transform _raycastHitPoint;
     [SerializeField] private Transform _planetTransform;
 
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private float _rotationSpeed = 60f;
 
-    private float _verticalCarPlaceOffset = .1f;
-
     private float _rotationAngle;
+    private float _radius;
 
-    private Vector3 _moveDirection;
     private RaycastHit _hit;
+
+    private void Start()
+    {
+        _radius = GetRadius();
+    }
 
     private void Update()
     {
         SetDirectionAndRotation();
-        PlaceCar();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGroundPosition();
     }
 
     private void SetDirectionAndRotation()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis(HORIZONTAL);
 
         _rotationAngle = Mathf.Clamp(_rotationAngle, -45f, 45f);
-        _moveDirection = new Vector3(0f, 0f, vertical).normalized;
 
         Vector3 eulerRotation = transform.eulerAngles;
 
@@ -38,33 +46,16 @@ public class CarController : MonoBehaviour
             _rotationAngle += Time.deltaTime * _rotationSpeed * horizontal;
         }
 
-        //transform.rotation = Quaternion.Euler(eulerRotation);
+        // transform.rotation = Quaternion.Euler(eulerRotation);
 
-        transform.Translate(transform.forward * Time.deltaTime * 10f);
-
-        Vector3 normalizedVectorRotation = GetNormalizedVerticalRotation();
         Quaternion targetRotation = Quaternion.FromToRotation(transform.up, (transform.position - _planetTransform.position).normalized) * transform.rotation;
 
+        transform.position = new Vector3(transform.position.x, transform.position.y + _radius, transform.position.z);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 50f * Time.deltaTime);
+        transform.Translate(transform.forward * Time.deltaTime * _moveSpeed);
     }
 
-    private Vector3 GetNormalizedVerticalRotation()
-    {
-        Vector3 verticalRotation = Vector3.zero;
+    private float GetRadius() => (_planetTransform.position - transform.position).magnitude;
 
-        if (Physics.Raycast(_raycastHitPoint.position, -_raycastHitPoint.up, out _hit, 0.25f))
-        {
-            verticalRotation = (_hit.point - _raycastHitPoint.position).normalized;
-        }
-
-        return verticalRotation;
-    }
-
-    private void PlaceCar()
-    {
-        if (_hit.point.magnitude > 0f)
-        {
-            transform.position = _hit.point + transform.up * _verticalCarPlaceOffset;
-        }
-    }
+    private void CheckGroundPosition() => Physics.Raycast(_raycastHitPoint.position, -_raycastHitPoint.up, out _hit, 0.25f);
 }
