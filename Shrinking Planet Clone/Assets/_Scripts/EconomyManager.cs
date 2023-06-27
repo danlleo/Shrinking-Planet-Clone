f88a3ae4518c7d334b0d878e514ca07d3ee417f2
Daniel Andrews <daniel.korienko@gmail.com>
2023-06-27 11:42:31 +0300

    Displaying Money Amount + refactoring

--- Shrinking Planet Clone/Assets/_Scripts/EconomyManager.cs
+++ Shrinking Planet Clone/Assets/_Scripts/EconomyManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using static UnitWorkingState;
 
-public class EconomyManager : MonoBehaviour
+public class EconomyManager : Singleton<EconomyManager>
 {
     [SerializeField] private Transform _moneyIconPrefab;
     [SerializeField] private Transform _canvasUI;
@@ -11,7 +11,10 @@ public class EconomyManager : MonoBehaviour
 
     private int _totalCurrentMoneyAmount;
 
-    private float _moveDuration = .3f;
+    protected override void Awake()
+    {
+        base.Awake();
+    }
 
     private void Start()
     {
@@ -20,39 +23,10 @@ public class EconomyManager : MonoBehaviour
 
     private void UnitWorkingState_OnUnitReceivedPayment(object sender, UnitRecievedPaymentEventArgs e)
     {
-        Vector3 screenPosition = Camera.main.WorldToScreenPoint(MouseWorld.GetPosition());
-
-        Transform moneyIcon = Instantiate(_moneyIconPrefab, _canvasUI);
-
-        moneyIcon.transform.position = screenPosition;
-
-        StartCoroutine(MoveIconToFinalPosition(moneyIcon));
-
         AddMoneyToCurrentAmount(e.MoneyAmount);
-
-        print(_totalCurrentMoneyAmount);
     }
 
-    private void AddMoneyToCurrentAmount(int moneyAmount) => _totalCurrentMoneyAmount += moneyAmount; 
+    private void AddMoneyToCurrentAmount(int moneyAmount) => _totalCurrentMoneyAmount += moneyAmount;
 
-    private IEnumerator MoveIconToFinalPosition(Transform icon)
-    {
-        float elapsedTime = 0f;
-
-        Vector3 startPosition = icon.transform.position;
-        Vector3 endPosition = _moneyBoxUI.transform.position;
-
-        while (elapsedTime < _moveDuration)
-        {
-            elapsedTime += Time.deltaTime;
-
-            float t = Mathf.Clamp01(elapsedTime / _moveDuration);
-
-            icon.position = Vector3.Lerp(startPosition, endPosition, InterpolateUtils.EaseInOutQuart(t));
-
-            yield return null;
-        }
-
-        Destroy(icon.gameObject);
-    }
+    public int GetTotalCurrentMoneyAmount() => _totalCurrentMoneyAmount;
 }
---
+++ Shrinking Planet Clone/Assets/_Scripts/UI/MoneyBoxUI.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using static UnitWorkingState;
+
+public class MoneyBoxUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _moneyBoxUI;
+    [SerializeField] private Transform _moneyIconPrefab;
+    [SerializeField] private Transform _moneyIcon;
+    [SerializeField] private TextMeshProUGUI _moneyAmountText;
+
+    private float _moveDuration = .3f;
+
+    private void Start()
+    {
+        UnitWorkingState.OnUnitReceivedPayment += UnitWorkingState_OnUnitReceivedPayment;
+        DayManager.Instance.OnDayEnded += DayManager_OnDayEnded;
+    }
+
+    private void DayManager_OnDayEnded(object sender, System.EventArgs e)
+    {
+        Hide();
+    }
+
+    private void UnitWorkingState_OnUnitReceivedPayment(object sender, UnitRecievedPaymentEventArgs e)
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(MouseWorld.GetPosition());
+
+        Transform moneyIcon = Instantiate(_moneyIconPrefab, _moneyBoxUI.transform);
+
+        moneyIcon.transform.position = screenPosition;
+
+        StartCoroutine(MoveIconToFinalPosition(moneyIcon));
+
+        // Display total current money amount text
+        _moneyAmountText.text = EconomyManager.Instance.GetTotalCurrentMoneyAmount().ToString();
+    }
+
+    private IEnumerator MoveIconToFinalPosition(Transform icon)
+    {
+        float elapsedTime = 0f;
+
+        Vector3 startPosition = icon.transform.position;
+        Vector3 endPosition = _moneyIcon.position;
+
+        while (elapsedTime < _moveDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float t = Mathf.Clamp01(elapsedTime / _moveDuration);
+
+            icon.position = Vector3.Lerp(startPosition, endPosition, InterpolateUtils.EaseInOutQuart(t));
+
+            yield return null;
+        }
+
+        Destroy(icon.gameObject);
+    }
+
+    private void Show() => _moneyBoxUI.SetActive(true);
+
+    private void Hide() => _moneyBoxUI.SetActive(false);
+}
