3e0eb27fda45c22d2d8795e91bb5ae9a4f0b71ea
Daniel Andrews <daniel.korienko@gmail.com>
2023-07-03 13:14:30 +0300

    Added save system + refactoring

---
+++ Shrinking Planet Clone/Assets/_Scripts/SaveSystem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SaveSystem : Singleton<SaveSystem>
+{
+    private const int DEFAULT_COMPANY_RANK_POSITION = 100;
+    private const int DEFAULT_TOTAL_MONEY_AMOUNT = 0;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SaveMoneyAmount(int moneyAmount) => PlayerPrefs.SetInt(SaveSystemParams.TOTAL_MONEY_KEY, moneyAmount);
+
+    public int LoadMoneyAmount()
+    {
+        if (PlayerPrefs.HasKey(SaveSystemParams.TOTAL_MONEY_KEY))
+        {
+            return PlayerPrefs.GetInt(SaveSystemParams.TOTAL_MONEY_KEY);
+        }
+
+        return DEFAULT_TOTAL_MONEY_AMOUNT;
+    }
+
+    public void SaveCompanyRankPosition(int companyRankPosition) => PlayerPrefs.SetInt(SaveSystemParams.COMPANY_RANK_POSITION_KEY, companyRankPosition);
+
+    public int LoadCompanyRankPosition()
+    {
+        if (PlayerPrefs.HasKey(SaveSystemParams.COMPANY_RANK_POSITION_KEY))
+        {
+            return PlayerPrefs.GetInt(SaveSystemParams.COMPANY_RANK_POSITION_KEY);
+        }
+
+        return DEFAULT_COMPANY_RANK_POSITION;
+    }
+}
---
+++ Shrinking Planet Clone/Assets/_Scripts/SaveSystemParams.cs
@@ -0,0 +1,5 @@
+public class SaveSystemParams
+{
+    public const string TOTAL_MONEY_KEY = "TotalMoney";
+    public const string COMPANY_RANK_POSITION_KEY = "CompanyRankPosition";
+}
